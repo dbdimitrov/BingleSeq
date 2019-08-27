8504528594e450cf75f06d5002f762f7a14df0a3
dbdimitrov <50865230+dbdimitrov@users.noreply.github.com>
2019-08-27 15:27:08 +0300

    Add files via upload
    Added Rank-based consensus functionality to CompTab

--- R/bulk_compDataTab.R
+++ R/bulk_compDataTab.R
@@ -7,7 +7,8 @@ bulk_compDataUI <- function(id) {
 
   tagList(
     sidebarPanel(
-      h4("Generate Venn Diagram"),
+
+      h4("Initialize by Running the DE Pipelines"),
 
       actionButton(ns("comparisonButton"), "Run DE Pipelines"),
 
@@ -15,27 +16,59 @@ bulk_compDataUI <- function(id) {
         condition = "input.comparisonButton > 0",
         ns = ns,
 
-        selectInput(
-          ns("selectIntersect"),
-          label = ("Select intersect of interest"),
-          choices = list(
-            "DESeq2 (only)" = "a1",
-            "DESeq2 & edgeR" = "a2",
-            "edgeR (only)" = "a3",
-            "DESeq2 & limma" = "a4",
-            "All Three Packages" = "a5",
-            "edgeR & limma" = "a6",
-            "limma (only)" = "a7"
-          )
+        tags$hr(),
+
+        h4("Generate Venn Diagram"),
+
+        actionButton(ns("vennButton"), "Generate Venn Diagram"),
+
+        conditionalPanel(condition =  "input.vennButton > 0",
+                         ns = ns,
+
+                         selectInput(
+                           ns("selectIntersect"),
+                           label = ("Select intersect of interest"),
+                           choices = list(
+                             "DESeq2 (only)" = "a1",
+                             "DESeq2 & edgeR" = "a2",
+                             "edgeR (only)" = "a3",
+                             "DESeq2 & limma" = "a4",
+                             "All Three Packages" = "a5",
+                             "edgeR & limma" = "a6",
+                             "limma (only)" = "a7"
+                           )
+                         ),
+
+                         downloadButton(ns("downloadIntersect"), "Download Intersect")
         ),
 
-        downloadButton(ns("downloadIntersect"), "Download Intersect")
+        tags$hr(),
+
+        h4("Produce Ranking Consesus"),
+
+        actionButton(ns("rankButton"), "Generate Ranking Table")
       )
     ),
 
     mainPanel(
-      htmlOutput(ns("helpCompInfo")),
-      plotOutput(ns("comparsionPlot"), width = "800px", height = "500px")
+
+      tabsetPanel(
+        id = ns("compMainTabSet"),
+        tabPanel(
+          title = "Venn Diagram",
+          value = "compPlotTab",
+          htmlOutput(ns("helpCompInfo")),
+          plotOutput(ns("comparsionPlot"), width = "800px", height = "500px")
+        ),
+        tabPanel(title = "Rank Table",
+                 value = "compTableTab",
+                 DT::dataTableOutput(ns("rankTable")),
+                 conditionalPanel(condition = "input.rankButton > 0",
+                                  ns = ns,
+                                  downloadButton(ns("downloadRank"), "Download Ranking Consensus")
+                 )
+        )
+      )
     )
   )
 }
@@ -58,8 +91,10 @@ bulk_compData <- function(input, output, session, rv, de) {
         between the different DE analysis packages.</b> </p> <br>
 
         Once the pipeline is finished a Venn Diagram with
-        the overlap between selected DE methods is returned. <br>
-        Each overlap(intersect) can then be downloaded <br> <br>
+        the overlap between selected DE methods can be generated <br>
+        Each overlap(intersect) can then be downloaded. <br>
+        Furthermore, a ranking consesus between the packages can be
+        generated and downloaded. <br><br>
 
         <i>Note that Differentially Expressed Genes are considered
         significant if FDR adjusted p-value < 0.05. <br>
@@ -74,8 +109,15 @@ bulk_compData <- function(input, output, session, rv, de) {
   observeEvent(input$comparisonButton, {
     show_waiter(tagList(spin_folding_cube(), h2("Loading ...")))
 
-    rv$xlist <-
-      getAllDE(rv$filteredCounts, de$conditionNo, de$replicateNo)
+    rv$allDE <- getAllDE(rv$filteredCounts, de$conditionNo, de$replicateNo)
+
+    hide_waiter()
+  })
+
+
+  observeEvent(input$vennButton, {
+
+    rv$xlist <- generateIntersect(rv$allDE)
 
     output$comparsionPlot <- renderPlot({
       grid.newpage()
@@ -83,8 +125,26 @@ bulk_compData <- function(input, output, session, rv, de) {
 
     })
 
-    hide_waiter()
+    updateTabsetPanel(session,
+                      "compMainTabSet",
+                      selected = "compPlotTab")
+
   })
+
+
+  observeEvent(input$rankButton, {
+
+    rv$consensus <- rankConsesus(rv$allDE[[2]], rv$allDE[[3]], rv$allDE[[1]], 2)
+
+    output$rankTable <-
+      DT::renderDataTable(rv$consensus)
+
+    updateTabsetPanel(session,
+                      "compMainTabSet",
+                      selected = "compTableTab")
+
+  })
+
 
 
   observeEvent(input$selectIntersect, {
@@ -106,6 +166,20 @@ bulk_compData <- function(input, output, session, rv, de) {
       write.csv(data, file, row.names = FALSE)
     }
   )
+
+  output$downloadRank <- downloadHandler(
+    filename = function() {
+      paste("RankingConsesus_",
+            ".csv",
+            sep = "")
+    },
+    content = function(file) {
+      data <- rv$consensus
+
+      write.csv(data, file, row.names = FALSE)
+    }
+  )
+
 }
 
 
@@ -122,9 +196,23 @@ getAllDE <- function(readCounts, conditionNo, replicateNo) {
   x2 <- deEdgeR(readCounts, conditionNo, replicateNo)
   x3 <- deLimma(readCounts, conditionNo, replicateNo)
 
-  x1_sig <- subset(x1, FDR < 0.05)
-  x2_sig <- subset(x2, FDR < 0.05)
-  x3_sig <- subset(x3, FDR < 0.05)
+  delist <- list(x1, x2, x3)
+
+  return(delist)
+}
+
+
+
+#' Extract Intersects
+#'
+#' @param deList list containing the DE Results of the 3 packages
+#' @export
+#' @return Returns a list with DE genes according to the different packages
+generateIntersect <- function(deList) {
+
+  x1_sig <- subset(deList[[1]], FDR < 0.05)
+  x2_sig <- subset(deList[[2]], FDR < 0.05)
+  x3_sig <- subset(deList[[3]], FDR < 0.05)
 
 
   DESeq_genes <- as.vector(rownames(x1_sig))
@@ -137,6 +225,7 @@ getAllDE <- function(readCounts, conditionNo, replicateNo) {
 
   return(list)
 }
+
 
 #' Generate a Venn Diagram with DE genes
 #'
@@ -174,4 +263,106 @@ getIntersect <- function(xlist, intersectID) {
   inter <- x[[intersectID]]
 
   return(inter)
+}
+
+
+#' Generates a ranking consesus from the selected methods
+#'
+#' @param x1_data DE method 1 results
+#' @param x2_data DE method 2 results
+#' @param x2_data DE method 3 results
+#' @param pipeline The pipeline used - scRNA-Seq or Bulk
+#' @export
+#' @return Returns a dataframe with the ranking consensus
+rankConsesus <- function(x1_data, x2_data, x3_data, pipeline){
+
+
+  if(pipeline == 1){
+
+    #rename p_val_adj to FDR
+  }
+  x1_data$X <- row.names(x1_data)
+  x2_data$X <- row.names(x2_data)
+  x3_data$X <- row.names(x3_data)
+
+
+  # order the data according to most significant
+  edgeR_ord <- x1_data[order(x1_data$FDR), ]
+  limma_ord <- x2_data[order(x2_data$FDR), ]
+  DESeq_ord <- x3_data[order(x3_data$FDR), ]
+
+
+
+
+
+  x1 <- as.vector(edgeR_ord$FDR) # extract FDR
+  names(x1) <- edgeR_ord$X # assign names
+  x1_ranked <- rank(x1) # produce ranks
+  x1_ranked <- x1_ranked[order(names((x1_ranked)))]
+
+  x2 <- as.vector(limma_ord$FDR)
+  names(x2) <- limma_ord$X
+  x2_ranked <- rank(x2)
+  x2_ranked <- x2_ranked[order(names((x2_ranked)))]
+
+  x3 <- as.vector(DESeq_ord$FDR)
+  names(x3) <- DESeq_ord$X
+  x3_ranked <- rank(x3)
+  x3_ranked <- x3_ranked[order(names((x3_ranked)))]
+
+
+  # combine the vectors into a
+  xdf <- as.data.frame((cbind(x1_ranked,x2_ranked,x3_ranked)))
+
+
+  # produce consesus
+  xdf$consensus <- (rowSums(xdf[,1:3])/3)
+
+
+  consesus <- as.vector(xdf$consensus) # extract consesus
+  names(consesus) <- row.names(xdf) # assign names
+  consesus
+
+
+  rerank <- rank(consesus)
+  rerank <- rerank[order(names((rerank)))]
+
+  # p.adj vectors
+  edgeR_padj <- as.vector(x1_data$FDR)
+  names(edgeR_padj) <- x1_data$X
+  edgeR_padj <- edgeR_padj[order(names((edgeR_padj)))]
+
+  limma_padj <- as.vector(x2_data$FDR)
+  names(limma_padj) <- x2_data$X
+  limma_padj <- limma_padj[order(names((limma_padj)))]
+
+  DESeq_padj <- as.vector(x3_data$FDR)
+  names(DESeq_padj) <- x3_data$X
+  DESeq_padj <- DESeq_padj[order(names((DESeq_padj)))]
+
+
+  # rebind the rankings + FDR for each package + consesus
+  xdf <- as.data.frame(cbind(x1_ranked, edgeR_padj,
+                             x2_ranked, limma_padj,
+                             x3_ranked, DESeq_padj,
+                             rerank))
+
+
+
+
+  xdf <- xdf[order(xdf$rerank),]
+
+  if(pipeline == 2){
+    colnames(xdf) <- c("edgeR Rank", "edgeR adj.p-value",
+                       "limma Rank", "limma adj.p-value",
+                       "DESeq2 Rank", "DESeq2 adj.p-value",
+                       "Ranking Consesus")
+  } else {
+    colnames(xdf) <- c("T-test Rank", "T-test adj.p-value",
+                       "Wilcoxon Rank", "Wilcoxon adj.p-value",
+                       "MAST Rank", "MAST adj.p-value",
+                       "Ranking Consesus")
+  }
+
+  return(xdf)
 }
